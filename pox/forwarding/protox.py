da9f114dec6aff195c54d1de8ecd6d4e150c9071
Gaurav Saraf <grv_srf@yahoo.co.in>
2014-11-17 19:26:37 -0500

    Latency finally done

--- pox/forwarding/protox.py
+++ pox/forwarding/protox.py
@@ -19,22 +19,31 @@ Start and run proto-x on OpenFlow Switches.
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.util import dpidToStr
+from pox.lib.util import dpidToStr, strToDPID
 import pox.lib.packet as pkt
 from pox.lib.recoco import Timer
 import time
 from pox.openflow.libopenflow_01 import *
-
+import calendar
+from time import gmtime
+ 
 log = core.getLogger()
 dpids = []
 ports = {}
 LAT_TYPE    = 0x07c3
 dpid_ts = {}
 dpid_latency = {}
-
+dpid_stats = {}
+mac = 0
+latency = 1
+bw = 2
+rx = 3
+tx= 4
+time_init = int(time.time())
+######################################################################################################
 def _handle_ConnectionUp (event):
   """
-  Tell all switches to forwards latency packets to controller
+  Tell all switches to forward latency packets to controller
   """
   print "Connected to switch",dpidToStr(event.dpid)
   SwHandler(event)
@@ -45,6 +54,8 @@ def _handle_ConnectionUp (event):
   msg.match = match
   msg.actions.append(of.ofp_action_output(port = of.OFPP_CONTROLLER))
   connection.send(msg)
+
+######################################################################################################
 
 class SwHandler(object):
   
@@ -58,46 +69,34 @@ class SwHandler(object):
     print "Add ports to link status DB"
     dpid_ts[self.dpid] = 0.000
     dpid_latency[self.dpid] = 0.000
+    dpid_stats[self.dpid] = []
     for p in event.ofp.ports:
       port = [str(p.hw_addr), 0.0, 100, 0, 0]
       ports[dpidToStr(event.dpid)][p.port_no] = port
 
-    core.openflow.addListenerByName("PacketIn", self.handle_pkt) 
-    core.openflow.addListenerByName("SwitchDescReceived", self.handle_switch_desc) 
+    #core.openflow.addListenerByName("PacketIn", self.handle_pkt) 
+    #core.openflow.addListenerByName("SwitchDescReceived", self.handle_switch_desc) 
+    #core.openflow.addListenerByName("PortStatsReceived", self.handle_PortStatsReceived)
 
-  def handle_pkt (self, event):
-    """
-    Handle incoming latency packets
-    """
-    packet = event.parsed
-#    print packet
-#    print packet.effective_ethertype
 
-    if packet.effective_ethertype == LAT_TYPE:
-      port = packet.src
-      [prevtime, mac, swdpdest, swdpsrc] = packet.payload.split(',')
-      prevtime = float(prevtime)
-      currtime = time.time()
-      dest_dpid = dpidToStr(event.dpid)
-      src_dpid = dpidToStr(self.dpid)
-      #print src_dpid,"-------",dest_dpid
-      #print swdpsrc,"-------",swdpdest
-      if src_dpid == swdpsrc and dest_dpid == swdpdest:
-        #print "DPID matched"
-        latency = round(((currtime - prevtime)*1000), 4) - dpid_latency[self.dpid] - dpid_latency[event.dpid]
-        #swd = ports[dpidToStr(self.dpid)]
-        swd = ports[swdpsrc]
-        for k in swd:
-          if swd[k][0] == mac:
-            break
-        ports[swdpsrc][k][1] = latency
-        print swdpsrc,"->",mac,"--",latency
+def handle_switch_desc(event):
+  currtime = time.time()
+  prevtime = dpid_ts[event.dpid]
+  latency = round(((currtime - prevtime)*1000), 4)
+  dpid_latency[event.dpid] = latency/2
 
-  def handle_switch_desc(self, event):
-    currtime = time.time()
-    prevtime = dpid_ts[event.dpid]
-    latency = round(((currtime - prevtime)*1000), 4)
-    dpid_latency[event.dpid] = latency/2
+def handle_PortStatsReceived (event):
+    
+    #Handles port stats event.
+    
+  #self.stats = event.stats
+  print "Switch",dpidToStr(event.dpid)
+  log.debug("PortStatsReceived")
+  for portStats in event.stats:
+    ports[dpidToStr(event.dpid)][portStats.port_no][rx] = portStats.rx_dropped
+    ports[dpidToStr(event.dpid)][portStats.port_no][tx] = portStats.tx_dropped
+
+######################################################################################################
 
 def find_latency(dpid):
   for key in ports[dpidToStr(dpid)]:
@@ -106,6 +105,8 @@ def find_latency(dpid):
       packet.data = create_lat_pkt(dpid,key,ports[dpidToStr(dpid)][key][0])
       #print "Sending to ",dpid," key ", key
       core.openflow.sendToDPID(dpid, packet)
+
+######################################################################################################
 
 def create_lat_pkt(dpid, port, port_mac):
   pkt1 = pkt.ethernet(type=LAT_TYPE)
@@ -117,34 +118,70 @@ def create_lat_pkt(dpid, port, port_mac):
         pkt1.payload = str(time.time()) + ',' + port_mac + ',' + dpidToStr(l.dpid2) + ',' + dpidToStr(l.dpid1)
         return pkt1.pack()
 
+######################################################################################################
 def find_latency_to_dpid(dpid):
   pkt = ofp_stats_request(type=OFPST_DESC)
   dpid_ts[dpid] = time.time()
-  core.openflow.sendToDPID(dpid, pkt)  
+  core.openflow.sendToDPID(dpid, pkt)
+  mbody = ofp_port_stats_request()
+  mbody.port_no = of.OFPP_NONE
+  pkt = ofp_stats_request(body = mbody)
+  pkt.type = OFPST_PORT
+  core.openflow.sendToDPID(dpid,pkt)
+
+######################################################################################################
 
 def find_latency1():
-  #print dpid_latency
+  print ports
   for dpid in dpids:
     find_latency_to_dpid(dpid)
-  #print ports
   for dpid in dpids:
     find_latency(dpid)
   #find_latency(dpids[0])
 #    print ports[dpidToStr(dpid)]
 
+#############################################################################################################################
+
+def handle_pkt (event):
+    """
+    Handle incoming latency packets
+    """
+    packet = event.parsed
+    if packet.effective_ethertype == LAT_TYPE:
+      print dpidToStr(event.dpid)
+      port = packet.src
+      [prevtime, mac, swdpdest, swdpsrc] = packet.payload.split(',')
+      prevtime = float(prevtime)
+      currtime = time.time()
+      #print "PrevTime = ", prevtime, "    CurrTime = ", currtime
+      dest_dpid = dpidToStr(event.dpid)
+      if dest_dpid == swdpdest:
+        #print "DPID matched"
+        latency = round((((currtime - prevtime)*1000) - dpid_latency[strToDPID(swdpsrc)] - dpid_latency[event.dpid]), 4)
+        #swd = ports[dpidToStr(self.dpid)]
+        swd = ports[swdpsrc]
+        for k in swd:
+          if swd[k][0] == mac:
+            break
+        if latency >=0:
+          ports[swdpsrc][k][1] = latency
+        print swdpsrc,"->",dest_dpid,"--",ports[swdpsrc][k][1]
+
+#############################################################################################################################
+
 Timer(10, find_latency1, recurring = True)
+
+######################################################################################################
 
 def launch ():
   from pox.openflow.discovery import launch
   launch()
   def start_launch ():
     core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
-#    core.openflow_discovery.addListenerByName("LinkEvent", _handle_LinkEvent)
+    core.openflow.addListenerByName("PacketIn", handle_pkt)
+    core.openflow.addListenerByName("SwitchDescReceived", handle_switch_desc)
+    core.openflow.addListenerByName("PortStatsReceived", handle_PortStatsReceived)
     print "Latency monitor"
     log.debug("Latency monitor running")
   core.call_when_ready(start_launch, "openflow_discovery") 
-#  core.openflow.addListenerByName("PortStatus", _handle_PortStatus)
-#  core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
-#  log.info("Latency monitor running.")
 
-#print "========================================================BLAH!!!============================================================"
